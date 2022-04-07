516584bc48f0c8354159be94e1a7804a36c60b5d
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-06 20:29:09 -0700

    create schema files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
+const connection = require('./config/connection');
 const cTable = require('console.table');
-const chalk = require('chalk');
 const figlet = require('figlet');
-
+const chalk = require('chalk');
+const validate = require('./javascript/validate');
 
